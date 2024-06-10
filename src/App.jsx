14a838f99ec691dde59d101555baecadc380cf23
iambasilp <iambasilp@gmail.com>
2024-06-10 14:52:13 +0530

    import components into main app component for learning what is self closing tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
     <div>
-      
+      <TodoInput />
+      <TodoList/>
+
     </div>
   )
 }
